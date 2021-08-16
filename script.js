dbd662c791edb1582cd95b9d2e6cabeef5f2db0f
Anderson Storqui <andersoncxszn@gmail.com>
2021-08-16 05:20:01 -0300

    resolvi o espaçamento do snapcrackle, mas não o prime

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function snapCrackle (maxValue){
     for(let i = 1; i <= maxValue;i++){
         
         if(i % 2 !== 0 && i % 5 === 0){
-            concatenar += 'Snap Crackle' + ', '
+            concatenar += 'SnapCrackle' + ', '
         }  
         else if(i % 2 !== 0){
             concatenar += 'Snap' + ', '
@@ -16,9 +16,7 @@ function snapCrackle (maxValue){
         } 
     } return concatenar
 }
-
-snapCrackle()
-
+snapCrackle(12)
 
 
 
@@ -26,46 +24,47 @@ snapCrackle()
 
 
 
-function verificandoNumerosPrimos (numeros){
-    let contador = 0
-    let guardarResultados = 0
-    //numeros = 5
-    //escrever um for pra dividir os numeros por todos os numeros
-    //verificar se só existem duas divisões perfeitas 
-    //5 / 1 = 5, 5 / 2 = 2.5 , 5 / 3  = 1.7 , 5 / 4 = 1.2, 5 / 5 = 1
-    for(let i = 1; i <= numeros; i++){
-        if(numeros % i === 0){
-            contador += 1
-        } 
-    } 
-    if(contador <= 2){
-        guardarResultados += contador
-    }
-    return guardarResultados
-}
+
+// function verificandoNumerosPrimos (numeros){
+//     let contador = 0
+//     let guardarResultados = 0
+//     //numeros = 5
+//     //escrever um for pra dividir os numeros por todos os numeros
+//     //verificar se só existem duas divisões perfeitas 
+//     //5 / 1 = 5, 5 / 2 = 2.5 , 5 / 3  = 1.7 , 5 / 4 = 1.2, 5 / 5 = 1
+//     for(let i = 1; i <= numeros; i++){
+//         if(numeros % i === 0){
+//             contador += 1
+//         } 
+//     } 
+//     if(contador <= 2){
+//         guardarResultados += contador
+//     }
+//     return guardarResultados
+// }
 
 
 
 
-function snapCracklePrime (maxValue){
-    let concatenar = ''
-    for(let i = 1; i <= maxValue;i++){
-        if(i % 2 !== 0 && i % 5 === 0){
-            concatenar += 'Snap Crackle' + ', '
-        }  
-        else if(i % 2 !== 0){
-            concatenar += 'Snap' + ', '
-        } 
-        else if (i % 5 === 0){
-            concatenar += ' Crackle' + ', '
-        }
-        else if(verificandoNumerosPrimos(i)){
-            concatenar += 'Prime' + ', '
-        }
-        else{
-            concatenar +=  i +  ', '
-        } 
-    } return concatenar
-}
-
-snapCracklePrime()
+// function snapCracklePrime (maxValue){
+//     let concatenar = ''
+//     for(let i = 1; i <= maxValue;i++){
+//         if(i % 2 !== 0 && i % 5 === 0){
+//             concatenar += 'SnapCrackle' + ', '
+//         }  
+//         else if(i % 2 !== 0){
+//             concatenar += 'Snap' + ', '
+//         } 
+//         else if (i % 5 === 0){
+//             concatenar += ' Crackle' + ', '
+//         }
+//         else if(verificandoNumerosPrimos(i)){
+//             concatenar += 'Prime' + ', '
+//         }
+//         else{
+//             concatenar +=  i +  ', '
+//         } 
+//     } return concatenar
+// }
+// console.log(snapCracklePrime(15))
+// snapCracklePrime(15)
